6f19234c965157f00a0823d606b38d626c47dde0
Weilbyte <git@weilbyte.dev>
2020-06-09 19:15:58 +0200

    Home page images

---
+++ back/front/src/data/text.tsx
@@ -0,0 +1,6 @@
+export const whoText : string = 
+`
+We are an English Arma 3 unit that focuses on serious milsim. Our core aspects are the player's enjoyment and immersion, scenario realism and the brotherhood between each and every single one of us. 
+<br/><br/>
+Just like our real life counterpart, we are heavily focused on light infantry, 
+`;
