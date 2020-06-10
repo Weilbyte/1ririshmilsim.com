cff1fa1181c0c8db9b7a667b33a5fb89dbe1971d
Weilbyte <git@weilbyte.dev>
2020-06-10 07:44:17 +0200

    Smaller text

--- back/front/src/component/consoleText.tsx
+++ back/front/src/component/consoleText.tsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 export class ConsoleText extends Component<{}> {
     componentDidMount() {
         console.log('%cHello there!', 'font-size: 42px; color: purple');
+        console.log('%cBetter not be pasting anything in here.', 'font-size: 20px; color: purple');
     }
 
     render() {
